7aaa61a6d098fb397e92d5fbfe73f77128ad4941
joparedesm <joparedesm@unal.edu.co>
2023-05-19 10:43:18 -0500

    guardarUp

---
+++ guardar.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $mensaje = $_POST['mensaje'];
+
+    // Ruta del archivo de texto donde se guardarán los datos
+    $archivo = 'contactos.txt';
+
+    // Crear el contenido del registro
+    $registro = "Nombre: $nombre\nEmail: $email\nTelefono : $telefono\nMensaje: $mensaje\n\n";
+
+    // Guardar el registro en el archivo de texto
+    file_put_contents($archivo, $registro, FILE_APPEND | LOCK_EX);
+
+    echo '¡Gracias por contactarnos!';
+} else {
+    echo 'Ocurrió un error al procesar el formulario.';
+}
+?>
